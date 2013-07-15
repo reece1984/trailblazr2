62a4de869bf8c0515ce39b8c9c8dbdf5f51e3050
James Reece <jamesreece@live.co.uk>
2013-07-15 15:42:48 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trailblazr2::Application.config.secret_token = 'bea014fe3fee07838428eeb212a8d5363d33266e6d1e900038718ff5ba38797b064795f0ebf58b1cd109643d113da129d789dea5ec30bc8d42d5e7ac2895d96d'
